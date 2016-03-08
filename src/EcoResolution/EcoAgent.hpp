696aa227ff4fdf06ffa0b6c2d5cdf52ae7920aee
damien <damien.pointin@insa-rouen.fr>
2016-03-08 13:46:24 +0000

    progression eco agent

--- src/EcoResolution/EcoAgent.hpp
+++ src/EcoResolution/EcoAgent.hpp
@@ -8,25 +8,25 @@ using namespace std;
 
 class EcoAgent{
     private:
-        vector<EcoAgent> but;
+        vector<EcoAgent*> but;
         Etat etat;
         bool agresse;
         bool gene;
     public:
         bool getAgresse();
         bool getGene();
-        Etat getEtat;
-        vector<EcoAgent> getBut();
+        Etat getEtat();
+        vector<EcoAgent*> getBut();
         void setAgresse(bool b);
         void setGene(bool b);
         void setEtat(Etat e);
-        void setbut(vector<EcoAgent> v);
-        void essayerSatisfaire();
-        vector<EcoAgent> trouverGeneur();
-        void faireSatisfaction();
-        void agresser(EcoAgent e);
-        void fuir(EcoAgent e);
-        EcoAgent trouverPlacePourFuir();
-        void faireFuite(EcoAgent e);
+        void setbut(vector<EcoAgent*> v);
+        void essayerSatisfaire(); 
+        virtual vector<EcoAgent*> trouverGeneur()=0; ///////REDEFINIE ligne
+        virtual void faireSatisfaction()=0;   //////////REDEFINIE cellule
+        virtual void agresser(EcoAgent* e)=0;    /////REDEFINIE ????????????
+        void fuir(EcoAgent* e);      
+        virtual EcoAgent* trouverPlacePourFuir()=0;         ////////////REDEFINIE cellule
+        virtual void faireFuite(EcoAgent* e)=0;             ////////REFEINIE cellule
 };
 #endif
