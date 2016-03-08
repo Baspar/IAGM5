f6617e49f16a063baeb952f47ea6495cf336391d
damien <damien.pointin@insa-rouen.fr>
2016-03-08 14:43:32 +0000

    ligne

---
+++ src/EcoResolution/LigneAgent.hpp
@@ -0,0 +1,14 @@
+#ifndef LIGNEAGENT_HPP
+#define LIGNEAGENT_HPP
+
+#include <vector>
+#include "Etat.hpp"
+
+using namespace std;
+
+class LigneAgent : public EcoAgent{
+    private:
+        int numero;
+        
+};
+#endif
