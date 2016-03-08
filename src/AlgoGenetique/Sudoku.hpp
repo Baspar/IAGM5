646724b7b402867779c32f77e79383e9e7af95b5
Laine Bastien <blaine@asi-testing>
2016-03-08 15:05:58 +0100

    [add] Algogen

--- src/AlgoGenetique/Sudoku.hpp
+++ src/AlgoGenetique/Sudoku.hpp
@@ -11,12 +11,20 @@ using namespace std;
 class Sudoku{
     private:
         vector<vector<Cell>> grid;
+        int scoreLigne(int i);
+        int scoreCol(int i);
     public:
         Sudoku();
         Sudoku(const Sudoku& sudoku);
+        Sudoku(const Sudoku& sudoku, const bool& copyGuess);
         void setValue(int x, int y, int val);
         int getValue(int x, int y) const;
         Cell getCell(int x, int y) const;
         void remplir();
+        int fitness();
+        pair<Sudoku, Sudoku> operator*(const Sudoku& sudo) const;
+        void afficher() const;
+        int scoreLignes(int i);
+        int scoreCols(int i);
 };
 #endif
