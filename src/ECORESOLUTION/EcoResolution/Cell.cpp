26ae208b129493d75410278ccfbac33e63b289ee
dpointin <damien.pointin@insa-rouen.fr>
2016-03-21 23:44:49 +0000

    vous avez eu peur ? :D

---
+++ src/ECORESOLUTION/EcoResolution/Cell.cpp
@@ -0,0 +1,111 @@
+#include "Cell.hpp"
+#include <math.h>
+void Cell::setSudoku(Sudoku* sud){
+    sudoku = sud;
+}
+Cell::Cell( int xc, int yc){//DONE
+    sudoku=nullptr;
+    x=xc;
+    y=yc;
+    /* if(trouverGeneur().size()==0){
+       gene=false;
+       etat=Etat::SATISFACTION;
+       }
+       else {
+       gene=true;
+       etat=Etat::RECHERCHE_SATISFACTION;
+       }*/
+}
+Sudoku* Cell::getSudoku(){
+    return sudoku;
+}
+Cell::Cell(Number num):number(num){//DONE
+}
+Cell::Cell(const Cell& cell):number(cell.getNumber()){//DONE
+}
+Number Cell::getNumber() const{//DONE
+    return number;
+}
+CellType Cell::getType() const{//DONE
+    return number.getCellType();
+}
+void Cell::setNumber(const Number& num){//DONE
+    number=num;
+}
+void Cell::setValue(int val){//DONE
+    number.setValue(val);
+}
+int Cell::getValue() const{//DONE
+    return number.getValue();
+}
+
+vector<EcoAgent*> Cell::trouverGeneur(){//WIP
+    vector<EcoAgent*> v=but[0]->trouverGeneur();
+    for(int j=0;j<but[1]->trouverGeneur().size();j++)
+        v.push_back(but[1]->trouverGeneur()[j]);
+    return v;
+}
+
+void Cell::faireSatisfaction(){//WIP
+    if( (but[0]->getEtat()==Etat::SATISFACTION) && (but[1]->getEtat()==Etat::SATISFACTION) )
+        etat==Etat::SATISFACTION;
+}
+
+void Cell::agresser(EcoAgent* e){//WIP
+    e->fuir(this);
+}
+
+
+EcoAgent* Cell::trouverPlacePourFuir(EcoAgent* e){//WIP
+    //cout << "trouver1"<< endl;
+    //cout << sudoku << endl;
+    int blocLigne=x/(int) sqrt(sudoku->getTaille());
+    int blocColonne=y/(int) sqrt(sudoku->getTaille());
+    bool b=false;
+    int nvx;
+    int nvy;
+    set<pair<int,int> > casePossible;
+    for(int i=0; i<(int) sqrt(sudoku->getTaille());i++){
+        for(int j=0; j<(int) sqrt(sudoku->getTaille());j++){
+            nvx=(int) sqrt(sudoku->getTaille())*blocLigne+i;
+            nvy=(int) sqrt(sudoku->getTaille())*blocColonne+j;
+            if ((!( ((nvx) ==x) && ((nvy)==y))) && sudoku->getCell(nvx,nvy)->getType()!=CellType::GIVEN && sudoku->getCell(nvx,nvy)->getEtat()!=Etat::SATISFACTION)
+                casePossible.insert(make_pair(nvx,nvy));
+        }
+    }
+    int chance=rand() %100;
+    if(casePossible.size()==0 || chance >70){
+        while (!b){
+            int alea=rand() %(int) sqrt(sudoku->getTaille());
+            //cout << "alea " << alea << endl;
+            int alea2=rand() %(int) sqrt(sudoku->getTaille());
+            //cout << "alea2 " << alea2 << endl;
+            nvx=(int) sqrt(sudoku->getTaille())*blocLigne+alea;
+            //cout << "nvx " << nvx << endl;
+            nvy=(int) sqrt(sudoku->getTaille())*blocColonne+alea2;
+            //cout << "nvy " << nvy << endl;
+            if ((!( ((nvx) ==x) && ((nvy)==y))) && sudoku->getCell(nvx,nvy)->getType()!=CellType::GIVEN )
+                b=true;
+            //cout << "test" << endl;
+        }
+    }
+    else{
+        int alea=rand() %(casePossible.size());
+        set<pair<int,int> >::const_iterator it=casePossible.begin();
+        for(int l=0; l<alea; l++)
+            it++;
+        nvx=it->first;
+        nvy=it->second;
+    }
+    sudoku->afficher(x, y, nvx, nvy);
+    return sudoku->getCell(nvx,nvy);
+}
+
+
+void Cell::faireFuite(EcoAgent* e){//WIP
+    int n = ((Cell*)e)->getValue();
+    int n2 = this->getValue();
+    ((Cell*)e)->setValue(n2);
+    this->setValue(n);
+
+}
---
+++ src/ECORESOLUTION/EcoResolution/ColonneAgent.cpp
@@ -0,0 +1,74 @@
+#include "ColonneAgent.hpp"
+#include <set>
+
+int ColonneAgent::getNumero(){
+    return numero;
+}
+
+Sudoku* ColonneAgent::getSudoku(){
+    return sudoku;
+}
+
+void ColonneAgent::setNumero(int n){
+    numero=n;
+}
+
+void ColonneAgent::setSudoku(Sudoku* s){
+    sudoku=s;
+    for(int i=0; i<sudoku->getTaille();i++){
+        ajouterBut(sudoku->getCell(i,numero));
+        sudoku->getCell(i,numero)->ajouterBut(this);
+    }
+}
+
+ColonneAgent::ColonneAgent(int n){
+    sudoku=nullptr;
+    numero=n;
+    agresse=false;
+    gene=false;
+    etat=Etat::RECHERCHE_SATISFACTION;
+}
+
+ColonneAgent::ColonneAgent(){
+    numero=-1;
+    sudoku=NULL;
+}
+
+vector<EcoAgent*> ColonneAgent::trouverGeneur(){
+    vector<EcoAgent*> v;
+    set<EcoAgent*> cellules;
+    for(int i=0; i<sudoku->getTaille();i++){
+        for(int j=i+1; j<sudoku->getTaille();j++){
+            if (sudoku->getValue(i, numero)==sudoku->getValue(j,numero)){
+                if(sudoku->getCell(i, numero)->getType()==CellType::GUESS)
+                    cellules.insert(sudoku->getCell(i,numero));
+                if(sudoku->getCell(j, numero)->getType()==CellType::GUESS)
+                    cellules.insert(sudoku->getCell(j,numero));
+            }
+        }
+    }
+    set<EcoAgent*>::const_iterator it=cellules.begin();
+    for(int l=0; l<cellules.size(); l++){
+        v.push_back(*it);
+        it++;
+    }
+    return v;
+}
+
+
+void ColonneAgent::faireSatisfaction(){
+    etat=Etat::SATISFACTION;
+}
+
+
+void ColonneAgent::agresser(EcoAgent* e){
+    e->fuir(this);
+}
+
+EcoAgent* ColonneAgent::trouverPlacePourFuir(EcoAgent* e){
+    return this;
+}
+
+void ColonneAgent::faireFuite(EcoAgent* e){
+
+}
